6fa9ced6f0e8d3870ae603e984c91260a9fc5614
vanikkanen <vanikkanen@gmail.com>
2025-02-03 20:28:48 +0200

    Add a button to clear and change the grid size
    Button clears the old grid squares and creates new
    ones based on the user input which needs to be a
    number between 1 and 100. The grid remains the
    same size of 1024*1024px but the amount of squares
    within the grid is changed

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-const container = document.querySelector("#container")
-const squareNo = 16*16 //16*16 grid
-for (let i = 0; i < squareNo; i++) {
-    let gridSquare = document.createElement("div")
-    gridSquare.addEventListener("mouseover", (e) => {
-        gridSquare.style.background = "gray"
-    })
-    gridSquare.className = "gridSquare"
-    container.appendChild(gridSquare)
+function createSquares(squaresPerRow) {
+
+    const totalSquares = squaresPerRow*squaresPerRow
+    const squareSize = 1024/squaresPerRow
+
+    const container = document.querySelector("#container")
+    for (let i = 0; i < totalSquares; i++) {
+        let gridSquare = document.createElement("div")
+        gridSquare.addEventListener("mouseover", (e) => {
+            gridSquare.style.background = "gray"
+        })
+        gridSquare.className = "gridSquare"
+        gridSquare.style.width = `${squareSize}px`
+        gridSquare.style.height = `${squareSize}px`
+        container.appendChild(gridSquare)
+    }
 }
 
+createSquares(16)
 
+function getValidInput() {
+    let input = null
+    while (input === null || isNaN(input) || input > 100 || input < 1) {
+        input = prompt("Please insert the new amount of squares per row. Max 100.")
+
+        if (input === null) {
+            alert("You must enter a number")
+            continue
+        }
+
+        input = Number(input)
+
+        if (isNaN(input) || input > 100 || input < 1) {
+            alert("Invalid input! Please enter a number between 1 and 100.")
+        }
+    }
+    return input
+}
+
+function resetGrid() {
+    
+    const input = getValidInput()
+    
+    const container = document.querySelector("#container")
+    while(container.lastChild) {
+        container.lastChild.remove()
+    }
+
+    createSquares(input)
+}

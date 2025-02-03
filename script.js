b5c0198ead9a517f5b327b48ed7129c450bedd2b
vanikkanen <vanikkanen@gmail.com>
2025-02-03 19:51:41 +0200

    Add mouseover event listener to the grid squares to change their color

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const container = document.querySelector("#container")
 const squareNo = 16*16 //16*16 grid
 for (let i = 0; i < squareNo; i++) {
     let gridSquare = document.createElement("div")
+    gridSquare.addEventListener("mouseover", (e) => {
+        gridSquare.style.background = "gray"
+    })
     gridSquare.className = "gridSquare"
     container.appendChild(gridSquare)
 }
+
+
